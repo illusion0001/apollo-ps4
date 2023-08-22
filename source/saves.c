4d56f40829d76a9d3b667b9316ed3783e9874a80
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-08-21 23:31:19 -0500

    Update saves.c

--- source/saves.c
+++ source/saves.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-#include <malloc.h>
 #include <string.h>
-#include <assert.h>
-#include <unistd.h>
-#include <time.h>
 #include <dirent.h>
+#include <time.h>
+#include <stdbool.h>
+#include <orbis/NetCtl.h>
 #include <orbis/SaveData.h>
+#include <orbis/UserService.h>
+#include <orbis/libkernel.h>
+#include <orbis/CommonDialog.h>
+#include <orbis/MsgDialog.h>
+#include <orbis/ImeDialog.h>
+#include <polarssl/md5.h>
 #include <sqlite3.h>
 
 #include "saves.h"
 #include "menu.h"
 #include "common.h"
-#include "sfo.h"
-#include "settings.h"
 #include "util.h"
+#include "sfo.h"
 
 #define UTF8_CHAR_STAR		"\xE2\x98\x85"
 
